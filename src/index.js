2f2efc719e7cd894ffad3543cb1ad6007022359e
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2024-06-16 18:32:43 +0700

    request post done

--- src/index.js
+++ src/index.js
@@ -1,47 +1,57 @@
 const express = require('express');
-const mysql = require('mysql2');
 const axios = require('axios');
+const mysql = require("mysql2");
 const app = express();
 
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'credit_db'
+    host: "34.101.207.105",
+    user: "root",
+    password: "bangkit",
+    database: "creditapp_db",
 });
 
 db.connect((err) => {
     if (err) {
-        console.error('Database connection error:', err);
+        console.error("Database connection error:", err);
     } else {
-        console.log('Connected to the database');
+        console.log("Connected to the database");
     }
 });
 
 app.post('/prediction', async (req, res) => {
-    const { id } = req.body;
+    const prediction = req.body
+    try {
+        const response = await axios.post('https://creditapp-64tbubeb5q-et.a.run.app/predict', prediction);
 
-    db.query('SELECT * FROM CreditApplication WHERE id = ?', [id], async (err, results) => {
-        if (err) {
-            res.status(500).send('Database query error');
-        } else if (results.length === 0) {
-            res.status(404).send('Credit application not found');
+        const predictionStatus = response.data.prediction[0];
+
+        let resultText;
+        if (predictionStatus === 0) {
+            resultText = "Denied";
+        } else if (predictionStatus === 1) {
+            resultText = "Accept";
         } else {
-            const application = results[0];
-
-            try {
-                const response = await axios.post('http://localhost:5000/predict', application);
-                res.json(response.data);
-            } catch (error) {
-                res.status(500).send('Prediction service error');
-            }
+            resultText = "Unknown";
         }
-    });
+
+        res.json({ prediction: resultText });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Something went wrong');
+    }
 });
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.get('/getPrediction', async (req, res) => {
+
+})
+
+app.get('/', async (req, res) => {
+    res.json("Welcome Credit Prediction APi")
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
